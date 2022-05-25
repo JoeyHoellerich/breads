b99150da26331a2a01daea6cf8d1a035eda23e07
Joey <joeyhoellerich@gmail.com>
2022-05-25 18:30:44 -0400

    added comments

--- controllers/bread_controller.js
+++ controllers/bread_controller.js
@@ -1,12 +1,18 @@
 // create contoller for breads
 const breads = require("express").Router();
 
-// INDEX - get list of bread (from JSON data)
+// INDEX - JSON data with all bread info
 const Bread = require("../models/bread.js")
 
-// GET /breads
+// GET /breads - all breads [url.com/breads]
 breads.get("/", (req, res) => {
     res.send(Bread);
+})
+
+// GET specific bread - 1 bread [url.com/breads/:arrayIndex]
+breads.get("/:arrayIndex", (req, res) => {
+    // whatever the number at the end of the url, get that array index from data and display it
+    res.send(Bread[req.params.arrayIndex]);
 })
 
 // export module
